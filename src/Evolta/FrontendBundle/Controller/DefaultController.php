e44f13188ae0e27e51291fa7bd2cd7cf8aa51fbf
Alejandro Arranz <alejandroarranz@gmail.com>
2012-02-12 21:23:29 +0100

    FrontendBundle

---
+++ src/Evolta/FrontendBundle/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Evolta\FrontendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction()
+    {
+        
+        return $this->render('FrontendBundle:Default:index.html.twig',
+                array(
+                    'name' => ''
+        ));
+    }
+    
+    public function staticAction($page) {
+        
+        return $this->render("FrontendBundle:Default:{$page}.html.twig",
+                array(
+                    'name' => ''
+        ));
+    }
+}
---
+++ src/Evolta/FrontendBundle/FrontendBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Evolta\FrontendBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FrontendBundle extends Bundle
+{
+}
